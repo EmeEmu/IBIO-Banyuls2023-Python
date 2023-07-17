9a2dc50b7976390bdbc95903a2d333899f1c23c0
Matteo Dommanget-Kott <matteo.d.kott@gmail.com>
2023-07-18 01:22:56 +0200

    added support for HDF5 attributs

--- Helper_Functions/accessing_data.py
+++ Helper_Functions/accessing_data.py
@@ -7,16 +7,29 @@ def h5tree_view(file):
     """
     import h5py
     assert type(file) == h5py._hl.files.File
-
+    
+    def view_h5attributes(obj, depth=0):
+        atts = obj.attrs
+        deep = "│   "*depth
+        ks = atts.keys()
+        for i,k in enumerate(ks):
+            if i == len(ks)-1:
+                d = deep + "└──"
+            else:
+                d = deep + "├──"
+            print(d, f'🏷️{k} = `{atts[k].decode("utf-8")}`')
+            
     def view_h5object(obj, depth=0):
         name = obj.name.split("/")[-1]
         deep = "│   "*depth + "├──"
         if type(obj) == h5py._hl.group.Group:
             print(deep,f"📁{name}")
+            view_h5attributes(obj, depth=depth+1)
             for k in list(obj):
                 view_h5object(obj[k], depth=depth+1)
         else:
             print(deep,f"🔢{name}", f"⚙️{obj.shape}{obj.dtype}")
+            view_h5attributes(obj, depth=depth+1)
 
     print(".", file.filename)
     for k in list(file):
